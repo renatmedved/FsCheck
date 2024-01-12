9e578858565f7ca20e4b33f244bc9a028dc502ce
Renat Musaev <renat@drive.net>
2024-01-12 13:06:43 +0400

    Add IPropertyConfigProviderAttribute example

---
+++ examples/FsCheck.XUnit.CSharpExamples/PropertyConfigProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using FsCheck.Fluent;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace FsCheck.XUnit.CSharpExamples;
+
+public class PropertyConfigProviderTest
+{
+    private class PropertyConfigProviderAttribute : FactAttribute, IPropertyConfigProviderAttribute
+    {
+        public PropertyAttribute GetPropertyTestConfig()
+        {
+            return new PropertyAttribute();
+        }
+    }
+    
+    [PropertyConfigProvider]
+    public Property ShouldWork_IPropertyConfigProviderAttribute()
+    {
+        return true.ToProperty();
+    }
+}
