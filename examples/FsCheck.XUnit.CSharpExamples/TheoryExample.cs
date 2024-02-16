a843b74c441358c00d30377dfa3448c10f8ed2bf
Renat Musaev <renat@drive.net>
2024-02-16 17:38:54 +0400

    Add example

---
+++ examples/FsCheck.XUnit.CSharpExamples/TheoryExample.cs
@@ -0,0 +1,15 @@
+﻿using FsCheck.Fluent;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace FsCheck.XUnit.CSharpExamples;
+
+public class TheoryExample
+{
+    [PropertyTheory]
+    [InlineData(42, "42")]
+    public Property Check(int a, string s)
+    {
+        return (a == 42 && s=="42").ToProperty();
+    }
+}
